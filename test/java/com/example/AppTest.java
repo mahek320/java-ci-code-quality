f6568fe30982fe17e199df21f2d6f2712461faf0
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-04 01:38:19 +0530

    Update AppTest.java

--- test/java/com/example/AppTest.java
+++ test/java/com/example/AppTest.java
@@ -1,1 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
 
